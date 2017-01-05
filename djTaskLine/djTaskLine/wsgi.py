ba78c23c7770984d10f6c30d3eb8eb2d08572d13
pogorelov-ss <pogorelov.ss@gmail.com>
2017-01-05 16:41:09 +0200

    settings folder and base dev prod

--- djTaskLine/djTaskLine/wsgi.py
+++ djTaskLine/djTaskLine/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djTaskLine.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djTaskLine.settings.dev")
 
 application = get_wsgi_application()
