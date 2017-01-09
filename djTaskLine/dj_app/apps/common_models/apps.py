b6d03b5fa08e641d6897a6abb44bcede6057bfda
pogorelov-ss <pogorelov.ss@gmail.com>
2017-01-09 11:50:47 +0200

    common_models app and dev libs

---
+++ djTaskLine/dj_app/apps/common_models/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CommonModelsConfig(AppConfig):
+    name = 'apps.common_models'
--- djTaskLine/dj_app/dj_main/settings/base.py
+++ djTaskLine/dj_app/dj_main/settings/base.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'opbeat.contrib.django',
+
+    'apps.common_models',
 ]
 
 MIDDLEWARE = [
