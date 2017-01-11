c6f29a9953e72a676471aa0d06706d3a20c73318
pogorelov-ss <pogorelov.ss@gmail.com>
2017-01-11 10:36:02 +0200

    temporary profile view

---
+++ djTaskLine/dj_app/apps/user_profiles/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url, include
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+]
--- djTaskLine/dj_app/apps/user_profiles/views.py
+++ djTaskLine/dj_app/apps/user_profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class UserProfileTemporaryView(TemplateView):
+    template_name = 'user_profiles/user-profile-temporary.html'
