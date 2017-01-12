22ec41ae68fc9bcbf2c5d26ce53c18ec41e47ec6
Lempiy <lempiyada@gmail.com>
2017-01-13 00:28:59 +0200

    Implement custom redirect

--- ngTaskLine/src/app/components/navbar/navbar.component.ts
+++ ngTaskLine/src/app/components/navbar/navbar.component.ts
@@ -3,27 +3,38 @@ import { PivotalService } from '../../services/pivotal.service';
 import { KottansLogoComponent } from '../kottans-logo/kottans-logo.component';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.sass']
+	selector: 'app-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.sass']
 })
 export class NavbarComponent implements OnInit, OnDestroy {
-  connections:Array<any>;
+	connections:Array<any>;
 
-  constructor(private pivotalService:PivotalService) {
-    this.connections = [];
-    this.connections.push(
-      this.pivotalService.onAllDataReady
-        .subscribe(() => console.log(this.pivotalService))
-    )
-  }
+	constructor(private pivotalService:PivotalService) {
+		this.connections = [];
+		this.connections.push(
+			this.pivotalService.onAllDataReady
+				.subscribe(() => console.log(this.pivotalService))
+		)
+	}
 
-  ngOnInit() {
+	ngOnInit() {
 
-  }
+	}
 
-  ngOnDestroy() {
-    this.connections.forEach(connection => connection.unsubscribe())
-  }
+	openPopUp(link:string, method:string) {
+		let newWindow = window.open(link, "_blank",
+		'menubar=no,toolbar=no,location=no,directories=no,status=no,scrollbars=no,resizable=no,dependent,width=450,height=500,left=600,top=100');
+		if (newWindow) newWindow.addEventListener('unload', (event)=> {
+			if (window.location.host === newWindow.location.host) {
+
+			}
+		});
+		return newWindow ? false : true; // allow the link to work if popup is blocked
+	}
+
+	ngOnDestroy() {
+		this.connections.forEach(connection => connection.unsubscribe())
+	}
 
 }
